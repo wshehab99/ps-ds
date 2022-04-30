a8983caf1bf4bb389f552bb791a027af10d1c680
Waleed Bin-Shehab <waleedbinshihab@gmail.com>
2022-04-30 05:58:32 +0200

    day 6

---
+++ hackerrank/day6/first_last_name.py
@@ -0,0 +1,8 @@
+def print_full_name(first, last):
+    full_name=first+" "+last
+    print( "Hello "+full_name+"! You just delved into python.")
+
+
+first=input()
+last=input()
+print_full_name(first,last)
