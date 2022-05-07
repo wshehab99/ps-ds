a3aff6f81e21d130ab381d33b73bf87535d71727
Waleed Bin-Shehab <waleedbinshihab@gmail.com>
2022-05-07 23:44:18 +0200

    Day 27

--- hackerrank/day29/exception.py
+++ hackerrank/day29/exception.py
@@ -1,1 +1,21 @@
-x=5
+from configparser import InterpolationError
+from msilib.schema import Error
+
+
+def exceptions_handling(a,b):
+    try:
+        x=a/b
+
+    except ZeroDivisionError as e:
+        print(f"Error Code: {e.args}")
+    except ValueError as e:
+        print(f"Error Code: {e.args}")
+
+
+
+n=int(input())
+for i in range(n):
+    
+    a,b=map(int,input().split())
+
+    exceptions_handling(a,b)
