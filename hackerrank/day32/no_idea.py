f6bb6e84e02a507ec2ae3a2000299bb77512e985
Waleed Bin-Shehab <waleedbinshihab@gmail.com>
2022-05-09 19:41:04 +0200

    Day 32

--- hackerrank/day32/no_idea.py
+++ hackerrank/day32/no_idea.py
@@ -2,10 +2,10 @@ n,m=map(int,input().split())
 array=list(map(int,input().split()))
 a=set(list(map(int,input().split())))
 b=set(list(map(int,input().split())))
-h=0
+happiness=0
 for item in array:
     if item in a:
-        h+=1
+        happiness+=1
     if item in b:
-        h-=1
-print(h)
+        happiness-=1
+print(happiness)
