1f77b0da0dfaf92db581aa023c410a97db047804
Waleed Bin-Shehab <waleedbinshihab@gmail.com>
2022-07-15 11:55:00 +0200

    day 50

---
+++ hackerrank/day50/maximize_it.py
@@ -0,0 +1,19 @@
+from itertools import product
+def max_it(k,m):
+    list_of=[]
+    for _ in range(k):
+        i=list(map(int,input().split()))
+        list_of.append(i)
+    list_of=list(product(*list_of))
+    results=[]
+    for i in list_of:
+        r=sum([a**2 for a in i])
+        r%=m
+        results.append(r)
+    result=max(results)
+    print(result)
+K,M=map(int,input().split())
+max_it(K,M)
+
+
+
