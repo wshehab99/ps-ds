a1251e4aa5cb2babe6106c5c2413f0d51e370dec
Waleed Bin Shehab <waleedbinshihab@gmail.com>
2022-12-11 23:21:16 +0200

    day80

--- hackerrank/day080/hex_color_code.py
+++ hackerrank/day080/hex_color_code.py
@@ -1,10 +1,19 @@
-import re
-n = int(input())
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from html.parser import HTMLParser
+class MyHTMLParser (HTMLParser):
+    def __handle_attrs(self,attrs):
+        for name,value in attrs:
+            print('->',name,'>',value)
+    
+    def handle_starttag(self, tag, attrs) :
+        print("Start",":",tag)
+        self.__handle_attrs(attrs)
+    def handle_endtag(self, tag,):
+        print("End".ljust(5),tag)
+    def handle_startendtag(self, tag, attrs):
+        print("Empty".ljust(5),":",tag)
+        self.__handle_attrs(attrs)
 
-for i in range(n):
-  s = input()
-  
-  w = s.split()
-  if len(w)>1 and '{' not in w:
-    w = re.findall(r'#[a-fA-F0-9]{3,6}', s)
-    [print(i) for i in w]
+
+MyParser = MyHTMLParser()
+MyParser.feed(''.join([input().strip() for i in range(int(input()))]))
